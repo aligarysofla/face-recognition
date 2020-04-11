12bc0554392a80c8fb51c3aaf0d92749c67a935e
aligarysofla <ali.garysofla@gmail.com>
2020-04-11 10:16:28 -0700

    comment

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navogation =() =>{
+    return(
+        
+        <nav className='flex justify-end'> 
+            <p className='f3 link dim black underline pa3 pointer'> Sign out</p>
+            {/* <p> Sign out</p>  */}
+        </nav>
+    );
+}
+
+export default Navogation;
