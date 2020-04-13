8629f0219d6f3196ff97c3ad146bb257be3bd103
aligarysofla <ali.garysofla@gmail.com>
2020-04-13 00:32:34 -0700

    com7

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-const Navogation =() =>{
-    return(
+const Navogation =({onRouteChange,isSignedIn}) =>{
+  
+            if (isSignedIn){
+                return(
+                    <nav className='flex justify-end'> 
+                        <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'> Sign out</p>
+                    </nav>
+                );
+            } else {
+                return(
+                    <nav className='flex justify-end'> 
+                        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'> Sign In</p>
+                        <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'> Register</p>
+                    </nav>
+                );
+            }
         
-        <nav className='flex justify-end'> 
-            <p className='f3 link dim black underline pa3 pointer'> Sign out</p>
-            {/* <p> Sign out</p>  */}
-        </nav>
-    );
+       
+    
 }
 
 export default Navogation;
